c2f2b6edf2196a9f81cc9d096ceaae68042700d1
Mohd Bilal <bilalmail7054@gmail.com>
2025-05-12 13:36:56 +0530

    Removed unused links

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -58,13 +58,8 @@ const Header = ({onMenuToggle = () => {}}: HeaderProps) => {
                <Button variant="ghost" size="icon" className="md:hidden hover:bg-red-700 text-white" onClick={onMenuToggle}>
                   <Menu className="w-6 h-6" />
                </Button>
-               <Link to="/" className="flex items-center gap-2">
-                  <div className="bg-white p-1 rounded-full">
-                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#dc2626" className="w-8 h-8">
-                        <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-                        <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75v4.5a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198c.03-.028.061-.056.091-.086L12 5.43z" />
-                     </svg>
-                  </div>
+               <Link to="/" className="flex justify-center items-center gap-2">
+                  <div className="bg-white rounded-full size-1"></div>
                   <h1 className="font-bold text-xl">RealtyPlus</h1>
                </Link>
             </div>
