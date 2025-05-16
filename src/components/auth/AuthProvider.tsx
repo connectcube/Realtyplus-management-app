17d5ab16dd1e2851e01c157ef0dfc42ce0067a66
Mohd Bilal <bilalmail7054@gmail.com>
2025-05-16 13:21:34 +0530

    Fixed global state mangement by setting up an active listener for userData

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -1,26 +1,23 @@
 // src/components/auth/AuthProvider.tsx
-import { useEffect } from "react";
-import { auth, fireDataBase } from "@/lib/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { useStore } from "@/lib/zustand";
+import { useEffect } from 'react';
+import { auth, fireDataBase } from '@/lib/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { useStore } from '@/lib/zustand';
 
 export const AuthProvider = ({ children }) => {
   const updateProfile = useStore.getState().setUser;
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async user => {
       if (user) {
-        console.log("User is signed in:", user.uid);
+        console.log('User is signed in:', user.uid);
 
         // Check each user type collection for the user's ID
-        const userTypes = ["landlords", "tenants", "contractors"];
+        const userTypes = ['landlords', 'tenants', 'contractors'];
 
         for (const userType of userTypes) {
-          const userQuery = query(
-            collection(fireDataBase, userType),
-            where("uid", "==", user.uid)
-          );
+          const userQuery = query(collection(fireDataBase, userType), where('uid', '==', user.uid));
 
           const querySnapshot = await getDocs(userQuery);
 
@@ -28,23 +25,21 @@ export const AuthProvider = ({ children }) => {
             const userData = querySnapshot.docs[0].data();
 
             updateProfile({
+              ...userData,
               userName:
-                userData.full_name ||
-                `${userData.firstName || ""} ${userData.lastName || ""}`,
+                userData.full_name || `${userData.firstName || ''} ${userData.lastName || ''}`,
               role: userType,
               email: userData.email,
               uid: userData.uid,
               id: querySnapshot.docs[0].id,
               phone: userData.phone,
             });
-
-            console.log("User data loaded from Firestore:", userType);
             break;
           }
         }
       } else {
         // User is signed out
-        console.log("User is signed out");
+        console.log('User is signed out');
         updateProfile({
           userName: null,
           role: null,
